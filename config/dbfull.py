ecc0c8a00f9df3af862d10edffd13ff0fd40ef95
RD.Galih Rakasiwi <rakasiwigalih947@gmail.com>
2023-08-09 15:10:58 +0700

    ignoring config

--- config/dbfull.py
+++
@@ -1,8 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-
-MONGO_URI ="mongodb://localhost:27017"
-
-async def get_database():
-    client = AsyncIOMotorClient(MONGO_URI)
-    database = client.get_database('dira_abinawa-all')
-    return database
