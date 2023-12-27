6030f25aabe1abb88a6f88120b5d2044ed9bcd29
galih752 <rd.galih752@gmail.com>
2023-12-27 18:01:59 +0700

    Update Data Potensi

--- routes/data_potensi.py
+++ routes/data_potensi.py
@@ -11,47 +11,72 @@ import pymongo
 dapot = APIRouter(tags=["Data Potensi"])
 
 @dapot.get('/')
-async def find_dapot(
-        current_user: UserInDB = Depends(get_current_user),
-    database: Collection = Depends(get_database)
-):
-    # Retrieve dapot details for the current user
-    db_dapot = database["dapot"]
-    dapot = await db_dapot.find_one({"gudep": current_user.gudep_number})
-
-    if not dapot:
-        raise HTTPException(status_code=404, detail="Dapot not found")
-
-    # Retrieve schools associated with the user's gudep number
+async def get_schools_with_potentials(database: Collection = Depends(get_database)):
+    # Retrieve all schools
     db_school = database["schools"]
-    gudep_cursor = db_school.find({"gudep": current_user.gudep_number})
+    schools = await db_school.find().to_list(length=None)
 
-    schools = []
-    async for school in gudep_cursor:
-        # Convert ObjectId to string
-        if "_id" in school:
-            school["_id"] = str(school["_id"])
-        schools.append(school)
+    # Retrieve all dapot data
+    db_dapot = database["dapot"]
+    all_dapot = await db_dapot.find().to_list(length=None)
 
-    # Prepare dapot details with opinions and school information
-    dapot_with_opinions = {
-        "school": schools,
-        "male_builder": dapot.get("male_builder", 0),
-        "female_builder": dapot.get("female_builder", 0),
-        "male_member": dapot.get("male_member", 0),
-        "female_member": dapot.get("female_member", 0),
-        "bantara_member": dapot.get("bantara_member", 0),
-        "laksana_member": dapot.get("laksana_member", 0),
-        "garuda_member": dapot.get("garuda_member", 0)
-    }
+    # Create a dictionary to store dapot data based on gudep
 
-    return JSONResponse(dapot_with_opinions)
+    # Prepare the result list
+    result = []
+
+    # Iterate through schools and check if dapot data exists
+    for school in schools:
+        gudep = school["gudep"]
+        dapot_data = next((dapot for dapot in all_dapot if dapot.get("gudep") == gudep), None)
+
+        if dapot_data is not None:
+            # If dapot data exists, use the existing data
+            result.append({
+                "school_name": school["school_name"],
+                "ambalan_name": school["ambalan_name"],
+                "level": school["level"],
+                "gudep": gudep,
+                "registration": school["registration"],
+                "dapot_data": {
+                    "male_builder": dapot_data.get("male_builder", 0),
+                    "female_builder": dapot_data.get("female_builder", 0),
+                    "male_member": dapot_data.get("male_member", 0),
+                    "female_member": dapot_data.get("female_member", 0),
+                    "bantara_member": dapot_data.get("bantara_member", 0),
+                    "laksana_member": dapot_data.get("laksana_member", 0),
+                    "garuda_member": dapot_data.get("garuda_member", 0),
+                }
+            })
+        else:
+            # If no dapot data exists, use default values (0)
+            result.append({
+                "school_name": school["school_name"],
+                "ambalan_name": school["ambalan_name"],
+                "level": school["level"],
+                "gudep": gudep,
+                "registration": school["registration"],
+                "dapot_data": {
+                    "male_builder": 0,
+                    "female_builder": 0,
+                    "male_member": 0,
+                    "female_member": 0,
+                    "bantara_member": 0,
+                    "laksana_member": 0,
+                    "garuda_member": 0,
+                }
+            })
+
+    return result
 
 @dapot.post('/')
-async def create_dapot(dapot: Dapot, database=Depends(get_database)):
+async def create_dapot(dapot: Dapot, database: Collection = Depends(get_database),current_user: UserInDB = Depends(get_current_user)):
+    print(database)  # Print or use a debugger to inspect the content
     db_dapot = database["dapot"]
     await db_dapot.insert_one(dapot.dict())
-    return dapot
+    existing_item = await db_dapot.find_one({"gudep": dapot.gudep})
+    if existing_item:
+        raise HTTPException(status_code=400, detail="Data already exists.")
 
 @dapot.put('/{id}')
 async def dapot_student(id, dapot: Dapot,database=Depends(get_database),curent_user = Depends(get_current_active_user)):
